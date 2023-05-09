0dd7da482cd60b463ba2d9293370fa8baef78221
Lee, Nam Gon <nglee.dev@gmail.com>
2023-05-09 22:01:17 +0900

    feat: comment entity 생성 (#1)

---
+++ src/main/java/com/enjoytrip/comment/entity/PostComment.java
@@ -0,0 +1,11 @@
+package com.enjoytrip.comment.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class PostComment extends Comment {
+}
---
+++ src/main/java/com/enjoytrip/comment/entity/ProductComment.java
@@ -0,0 +1,13 @@
+package com.enjoytrip.comment.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ProductComment extends Comment {
+
+    private Integer star;
+}
