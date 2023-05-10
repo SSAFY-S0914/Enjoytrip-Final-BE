4e8ba9a01056a31774c3c01bc2fd8879485dc56b
Ha Jung Ho <ggk14@naver.com>
2023-05-10 21:35:25 +0900

    feat: Member pagination 구현

--- src/main/java/com/enjoytrip/member/controller/MemberController.java
+++ src/main/java/com/enjoytrip/member/controller/MemberController.java
@@ -4,10 +4,14 @@ import com.enjoytrip.member.dto.MemberDto;
 import com.enjoytrip.member.entity.Member;
 import com.enjoytrip.member.mapper.MemberMapper;
 import com.enjoytrip.member.service.MemberService;
+import com.enjoytrip.utils.dtoUtils.MultiResponseDto;
+import com.enjoytrip.utils.dtoUtils.SingleResponseDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -36,7 +41,7 @@ public class MemberController {
         Member memberForResponse = service.createMember(memberForService);
         MemberDto.Response response = mapper.memberToMemberResponse(memberForResponse);
 
-        return new ResponseEntity(response, HttpStatus.CREATED);
+        return new ResponseEntity(new SingleResponseDto<>(response), HttpStatus.CREATED);
     }
 
     @PatchMapping("/{member-id}")
@@ -47,7 +52,7 @@ public class MemberController {
         Member memberForResponse = service.updateMember(memberForService);
         MemberDto.Response response = mapper.memberToMemberResponse(memberForResponse);
 
-        return new ResponseEntity(response, HttpStatus.OK);
+        return new ResponseEntity(new SingleResponseDto<>(response), HttpStatus.OK);
     }
 
     @GetMapping("/{member-id}")
@@ -55,14 +60,18 @@ public class MemberController {
         Member memberForResponse = service.findOneMember(memberId);
         MemberDto.Response response = mapper.memberToMemberResponse(memberForResponse);
 
-        return new ResponseEntity(response, HttpStatus.OK);
+        return new ResponseEntity(new SingleResponseDto<>(response), HttpStatus.OK);
     }
 
-//    @GetMapping
-//    public ResponseEntity getAllMembers(@Positive @RequestParam int page,
-//            @Positive @RequestParam int size) {
-//
-//    }
+    @GetMapping
+    public ResponseEntity getAllMembers(@Positive @RequestParam int page,
+            @Positive @RequestParam int size) {
+        Page<Member> pageMember = service.findAllMembers(page - 1, size);
+        List<Member> memberListForResponse = pageMember.getContent();
+        List<MemberDto.Response> response = mapper.membersToMemberList(memberListForResponse);
+
+        return new ResponseEntity(new MultiResponseDto<>(response, pageMember), HttpStatus.OK);
+    }
 
     //TODO 회원 상태 변경 로직
     @DeleteMapping("/{member-id}")
