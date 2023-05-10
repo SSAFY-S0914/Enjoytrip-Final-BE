34823cf9fab319d2ad71b82c50966e30900aac41
Ha Jung Ho <ggk14@naver.com>
2023-05-10 21:23:04 +0900

    feat: MemberController 구조 완성

--- src/main/java/com/enjoytrip/member/controller/MemberController.java
+++ src/main/java/com/enjoytrip/member/controller/MemberController.java
@@ -5,11 +5,17 @@ import com.enjoytrip.member.entity.Member;
 import com.enjoytrip.member.mapper.MemberMapper;
 import com.enjoytrip.member.service.MemberService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,11 +30,45 @@ public class MemberController {
     private MemberService service;
     private MemberMapper mapper;
 
-    public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post post) {
-        Member serviceMember = mapper.memberPostToMember(post);
-        Member responseMember = service.createMember(serviceMember);
-        MemberDto.Response response = mapper.memberToMemberResponse(responseMember);
+    @PostMapping
+    public ResponseEntity postMember(@Valid @RequestBody MemberDto.Post postRequest) {
+        Member memberForService = mapper.memberPostToMember(postRequest);
+        Member memberForResponse = service.createMember(memberForService);
+        MemberDto.Response response = mapper.memberToMemberResponse(memberForResponse);
 
         return new ResponseEntity(response, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(@Valid @RequestBody MemberDto.Patch patchRequest,
+            @Positive @PathVariable("member-id") long memberId) {
+        Member memberForService = mapper.memberPatchToMember(patchRequest);
+        memberForService.setId(memberId);
+        Member memberForResponse = service.updateMember(memberForService);
+        MemberDto.Response response = mapper.memberToMemberResponse(memberForResponse);
+
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{member-id}")
+    public ResponseEntity getOneMember(@PathVariable("member-id") long memberId) {
+        Member memberForResponse = service.findOneMember(memberId);
+        MemberDto.Response response = mapper.memberToMemberResponse(memberForResponse);
+
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
+//    @GetMapping
+//    public ResponseEntity getAllMembers(@Positive @RequestParam int page,
+//            @Positive @RequestParam int size) {
+//
+//    }
+
+    //TODO 회원 상태 변경 로직
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteOneMember(@PathVariable("member-id") long memberId) {
+        service.deleteOneMember(memberId);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
