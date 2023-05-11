b17cd86a1028b78efde546114e0692f099b1f952
Ha Jung Ho <ggk14@naver.com>
2023-05-12 00:25:39 +0900

    feat: Like Entity 작성

--- src/main/java/com/enjoytrip/like/entity/Like.java
+++ src/main/java/com/enjoytrip/like/entity/Like.java
@@ -1,4 +1,23 @@
 package com.enjoytrip.like.entity;
 
-public class Like {
+import com.enjoytrip.utils.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Like extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
