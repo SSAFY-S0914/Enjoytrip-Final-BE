d7f024fab2d5b37e57f3a43395d8cc0f0d75a794
이남곤 <nglee.dev@gmail.com>
2023-05-12 16:10:24 +0900

    feat: ProductSearchController 구현 및 ProductSearchService 생성

--- src/main/java/com/enjoytrip/product/service/ProductSearchService.java
+++ src/main/java/com/enjoytrip/product/service/ProductSearchService.java
@@ -1,7 +1,25 @@
 package com.enjoytrip.product.service;
 
+import com.enjoytrip.product.dto.ItemDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductSearchService {
+    public List<ItemDto> searchLocation(Map<String, String> queryParams) {
+    }
+
+    public List<ItemDto> searchKeyword(Map<String, String> queryParams) {
+    }
+
+    public List<ItemDto> searchFestival(Map<String, String> queryParams) {
+    }
+
+    public List<ItemDto> searchStay(Map<String, String> queryParams) {
+    }
+
+    public List<ItemDto> searchArea(Map<String, String> queryParams) {
+    }
 }
