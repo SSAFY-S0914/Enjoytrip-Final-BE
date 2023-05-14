5ca5bfabb44b2dc330adb33b6d87599a9170d764
이남곤 <nglee.dev@gmail.com>
2023-05-14 23:35:21 +0900

    feat: Post 구현 끝

--- src/main/java/com/enjoytrip/post/controller/PostController.java
+++ src/main/java/com/enjoytrip/post/controller/PostController.java
@@ -4,11 +4,17 @@ import com.enjoytrip.post.dto.PostDto;
 import com.enjoytrip.post.entity.Post;
 import com.enjoytrip.post.mapper.PostMapper;
 import com.enjoytrip.post.service.PostService;
+import com.enjoytrip.utils.dtoUtils.SingleResponseDto;
 import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,36 +27,50 @@ public class PostController {
 
     @PostMapping
     public ResponseEntity createPost(@RequestBody PostDto.Post postRequest) {
+        Post post = postMapper.postRequestToPost(postRequest);
+        postService.save(post);
+        return new ResponseEntity(CREATED);
     }
 
     @GetMapping("/{post-id}")
     public ResponseEntity findById(@PathVariable("post-id") Long postId) {
-
+        Post post = postService.findById(postId);
+        PostDto.Get result = postMapper.postToGetRequest(post);
+        return new ResponseEntity(new SingleResponseDto<>(result), OK);
     }
 
-    @PatchMapping("/{post-id}")
-    public ResponseEntity patchPost(@PathVariable("post-id") Long postId) {
-
+    @PatchMapping()
+    public ResponseEntity patchPost(@RequestBody PostDto.Patch patchRequest) {
+        Post post = postMapper.patchRequestToPost(patchRequest);
+        postService.save(post);
+        return new ResponseEntity(CREATED);
     }
 
     @DeleteMapping("/{post-id}")
     public ResponseEntity deletePost(@PathVariable("post-id") Long postId) {
-
+        postService.deleteById(postId);
+        return new ResponseEntity(ACCEPTED);
     }
 
     @GetMapping("/public")
     public ResponseEntity findAllByPublic() {
-
+        List<Post> posts = postService.findAllByPublic();
+        List<PostDto.Get> results = postMapper.postListToGetRequest(posts);
+        return new ResponseEntity(new SingleResponseDto<>(results), OK);
     }
 
     @GetMapping("/group/{group-id}")
     public ResponseEntity findAllByGroupId(@PathVariable("group-id") Long groupId) {
-
+        List<Post> posts = postService.findAllByGroupId(groupId);
+        List<PostDto.Get> results = postMapper.postListToGetRequest(posts);
+        return new ResponseEntity(new SingleResponseDto<>(results), OK);
     }
 
     @GetMapping("/group/member/{member-id}")
     public ResponseEntity findAllByMemberId(@PathVariable("member-id") Long memberId) {
-
+        List<Post> posts = postService.findAllByMemberId(memberId);
+        List<PostDto.Get> results = postMapper.postListToGetRequest(posts);
+        return new ResponseEntity(new SingleResponseDto<>(results), OK);
     }
 
 }
--- src/main/java/com/enjoytrip/post/mapper/PostMapper.java
+++ src/main/java/com/enjoytrip/post/mapper/PostMapper.java
@@ -3,9 +3,19 @@ package com.enjoytrip.post.mapper;
 import com.enjoytrip.post.dto.PostDto;
 import com.enjoytrip.post.entity.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
 
     Post postRequestToPost(PostDto.Post postRequest);
+
+    Post patchRequestToPost(PostDto.Patch patchRequest);
+
+    @Mapping(source = "post.writer.nickname", target = "nickname")
+    PostDto.Get postToGetRequest(Post post);
+
+    List<PostDto.Get> postListToGetRequest(List<Post> posts);
 }
