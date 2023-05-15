14f235f2fd206411e853b2eecd66a7850bb2c144
이남곤 <nglee.dev@gmail.com>
2023-05-15 12:08:03 +0900

    fix: @Repository 추가

--- src/main/java/com/enjoytrip/member/repository/MemberRepository.java
+++ src/main/java/com/enjoytrip/member/repository/MemberRepository.java
@@ -2,7 +2,9 @@ package com.enjoytrip.member.repository;
 
 import com.enjoytrip.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
