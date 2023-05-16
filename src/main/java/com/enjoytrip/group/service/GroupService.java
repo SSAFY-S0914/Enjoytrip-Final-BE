2ff853f10fc8eceb31b9ae7705523c70e37aa12f
이남곤 <nglee.dev@gmail.com>
2023-05-16 17:33:34 +0900

    feat: GroupService

--- src/main/java/com/enjoytrip/group/service/GroupService.java
+++ src/main/java/com/enjoytrip/group/service/GroupService.java
@@ -1,11 +1,43 @@
 package com.enjoytrip.group.service;
 
+import com.enjoytrip.group.entity.Group;
+import com.enjoytrip.group.mapper.GroupMapper;
+import com.enjoytrip.group.repository.GroupRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class GroupService {
+
+    private final GroupRepository groupRepository;
+    private final GroupMapper groupMapper;
+
+    public List<Group> findAll() {
+        return groupRepository.findAll();
+    }
+
+    public void save(Group group) {
+        groupRepository.save(group);
+    }
+
+    public Group findById(Long groupId) {
+        return groupRepository.findById(groupId).get();
+    }
+
+    public void deleteById(Long groupId) {
+        groupRepository.deleteById(groupId);
+    }
+
+    public void requestInvite(Long groupId, Long memberId) {
+
+    }
+
+    public void approveInvite(Long groupId, Long memberId) {
+
+    }
 
 }
 
