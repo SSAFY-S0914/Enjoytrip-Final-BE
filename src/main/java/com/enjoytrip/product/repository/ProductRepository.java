d6aaa811d3d627ef566bf4e198ff18b11f7a76d4
이남곤 <nglee.dev@gmail.com>
2023-05-16 10:34:48 +0900

    feat: ProductRepository, Entity

--- src/main/java/com/enjoytrip/product/repository/ProductRepository.java
+++ src/main/java/com/enjoytrip/product/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.enjoytrip.product.repository;
 
-public class ProductRepository {
+import com.enjoytrip.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
