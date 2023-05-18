6c8c2be450a07f3af7526d1033cc1f0b1317176a
Ha Jung Ho <ggk14@naver.com>
2023-05-18 16:58:43 +0900

    feat: 테스트를 위해 타 기능 배제(주석)

--- src/main/java/com/enjoytrip/group/mapper/GroupMapper.java
+++ src/main/java/com/enjoytrip/group/mapper/GroupMapper.java
@@ -15,10 +15,10 @@ public interface GroupMapper {
 
     List<GroupDto.Get> groupListToGetRequest(List<Group> group);
 
-    @Mapping(target = "memberId", ignore = true)
+//    @Mapping(target = "memberId", ignore = true)
     Group postRequestToGroup(GroupDto.Post postRequest);
 
-    @Mapping(target = "memberId", ignore = true)
-    @Mapping(target = "groupId", ignore = true)
+//    @Mapping(target = "memberId", ignore = true)
+//    @Mapping(target = "groupId", ignore = true)
     Group patchRequestToGroup(GroupDto.Patch patchRequest, @MappingTarget Group group);
 }
