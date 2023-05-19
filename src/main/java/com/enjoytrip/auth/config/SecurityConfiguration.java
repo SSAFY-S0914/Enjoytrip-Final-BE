3644b62912604d42096cd2158854923a51685fb7
Ha Jung Ho <ggk14@naver.com>
2023-05-19 17:49:04 +0900

    feat: Spring Security 설정

--- src/main/java/com/enjoytrip/auth/config/SecurityConfiguration.java
+++ src/main/java/com/enjoytrip/auth/config/SecurityConfiguration.java
@@ -2,15 +2,16 @@ package com.enjoytrip.auth.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import com.enjoytrip.auth.CustomAuthorityUtils;
-import com.enjoytrip.auth.JwtTokenizer;
-import com.enjoytrip.auth.OAuth2MemberSuccessHandler;
-import com.enjoytrip.auth.filter.JwtAuthenticationFilter;
+import com.enjoytrip.auth.JwtHandler.MemberAccessDeniedHandler;
+import com.enjoytrip.auth.JwtHandler.MemberAuthenticationFailureHandler;
+import com.enjoytrip.auth.JwtHandler.MemberAuthenticationSuccessHandler;
+import com.enjoytrip.auth.exception.MemberAuthenticationEntryPoint;
 import com.enjoytrip.auth.filter.JwtVerificationFilter;
-import com.enjoytrip.member.service.MemberService;
+import com.enjoytrip.auth.utils.CustomAuthorityUtils;
+import com.enjoytrip.auth.jwt.JwtTokenizer;
+import com.enjoytrip.auth.filter.JwtAuthenticationFilter;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -42,34 +43,39 @@ public class SecurityConfiguration {
 
     //Oauth 2_google 적용
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .headers().frameOptions().sameOrigin()
                 .and()
                 .csrf().disable()
                 .cors(withDefaults())
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)// 세션 정책 : 세션을 생성하지 않도록 설정 => JWT 사용 환경에선 세션을 사용하지 않음
+                .sessionManagement().sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS)// 세션 정책 : 세션을 생성하지 않도록 설정 => JWT 사용 환경에선 세션을 사용하지 않음(SecurityContext 정보를 얻을 때 Session 을 사용하지 않음)
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new MemberAuthenticationEntryPoint()) // 인증서 예외 발생 시 거치는 핸들러(401 Unauthorized)
+                .accessDeniedHandler(new MemberAccessDeniedHandler()) // 인증서는 정상이지만, 올바르지 않은 권한으로 요청 시 거치는 핸들러(403 Forbidden)
+                .and()
                 .apply(new CustomFilterConfigurer()) // 커스텀 필터 추가
                 .and()
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers(HttpMethod.POST, "/*/members").permitAll() // 회원 가입 : ALL
-                                .requestMatchers(HttpMethod.PATCH, "/*/members/**").hasRole("USER") // 회원 수정 : User
-                                .requestMatchers(HttpMethod.GET, "/*/members").hasRole("ADMIN") // 회원 정보 목록 : Admin
-                                .requestMatchers(HttpMethod.GET, "/*/members/**").hasAnyRole("USER", "ADMIN") // 회원 조회 : User, Admin
-                                .requestMatchers(HttpMethod.DELETE, "/*/members/**").hasRole("USER") // 회원 탈퇴 : User
-//                        .requestMatchers(HttpMethod.PATCH, "/*/coffees/**").hasRole("ADMIN") // SpringBoot 3 : antMatchers => requestMatchers
-//                        .requestMatchers(HttpMethod.GET, "/*/coffees/**").hasAnyRole("USER", "ADMIN")
-//                        .requestMatchers(HttpMethod.GET, "/*/coffees").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/hello-oauth2").permitAll()
-                        .anyRequest().permitAll()
+                                // SpringBoot 3 : antMatchers => requestMatchers
+                                .requestMatchers(HttpMethod.POST, "/members").permitAll() // 회원 가입 : ALL
+                                .requestMatchers(HttpMethod.PATCH, "/members/**")
+                                .hasRole("USER") // 회원 수정 : User
+                                .requestMatchers(HttpMethod.GET, "/members")
+                                .hasRole("ADMIN") // 회원 정보 목록 : Admin
+                                .requestMatchers(HttpMethod.GET, "/members/**")
+                                .hasAnyRole("USER", "ADMIN") // 회원 조회 : User, Admin
+                                .requestMatchers(HttpMethod.DELETE, "/members/**")
+                                .hasRole("USER") // 회원 탈퇴 : User
+                                .anyRequest().permitAll()
                 );
 //                .oauth2Login(oauth2 -> oauth2
 //                        .successHandler(new OAuth2MemberSuccessHandler(jwtTokenizer, authorityUtils, memberService))
 //                );
-
 
         return http.build();
     }
@@ -79,13 +85,13 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-    
+
     //CORS 처리
     @Bean
-    CorsConfigurationSource corsConfigurationSource(){
+    CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PATCH", "DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
 
@@ -93,16 +99,26 @@ public class SecurityConfiguration {
     }
 
     //CustomFilterConfigurer => custom filter 를 filter chain 에 추가 하기 위한 용도
-    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+    public class CustomFilterConfigurer extends
+            AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+
         @Override
         public void configure(HttpSecurity builder) throws Exception {
             AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class); //AuthenticationManager 객체를 얻음
+
+            //Authentication
             JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer); // JwtAuthenticationFilter 에 AuthenticationManager 와 JwtTokenizer 를 DI
             jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login"); // default URL : "/login"
-            builder.addFilter(jwtAuthenticationFilter);
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler()); // 인증 성공 시 실행할 핸들러 추가
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler()); // 인증 실패 시 실행할 핸들러 추가
 
-//            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
-//            builder.addFilterAfter(jwtVerificationFilter, OAuth2LoginAuthenticationFilter.class); // (2)
+            //Verification
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
+
+            builder
+                    .addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class); // JwtAuthenticationFilter 뒤에 jwtVerificationFilter 위치
+//            builder.addFilterAfter(jwtVerificationFilter, OAuth2LoginAuthenticationFilter.class); //
         }
     }
 
