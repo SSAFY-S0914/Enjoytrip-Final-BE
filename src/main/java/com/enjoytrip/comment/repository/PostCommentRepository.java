7d0f4401e5125f139d777cb6c1f065e3a1b26140
이남곤 <nglee.dev@gmail.com>
2023-05-19 10:13:15 +0900

    feat: Repository 연관관계로 검색

--- src/main/java/com/enjoytrip/comment/repository/PostCommentRepository.java
+++ src/main/java/com/enjoytrip/comment/repository/PostCommentRepository.java
@@ -4,6 +4,13 @@ import com.enjoytrip.comment.entity.PostComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
+
+    List<PostComment> findByPost_Id(Long postId);
+
+    List<PostComment> findByMember_Id(Long writerId);
+
 }
--- src/main/java/com/enjoytrip/comment/repository/ProductCommentRepository.java
+++ src/main/java/com/enjoytrip/comment/repository/ProductCommentRepository.java
@@ -4,6 +4,13 @@ import com.enjoytrip.comment.entity.ProductComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductCommentRepository extends JpaRepository<ProductComment, Long> {
+
+    List<ProductComment> findByPost_Id(Long productId);
+
+    List<ProductComment> findByMember_Id(Long writerId);
+
 }
