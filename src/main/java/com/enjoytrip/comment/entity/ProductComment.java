8722510bbac25567e4652d0dee8e41466a377c4c
이남곤 <nglee.dev@gmail.com>
2023-05-19 10:06:11 +0900

    feat: Comment와 Product, Post 연관관계

--- src/main/java/com/enjoytrip/comment/entity/ProductComment.java
+++ src/main/java/com/enjoytrip/comment/entity/ProductComment.java
@@ -1,6 +1,10 @@
 package com.enjoytrip.comment.entity;
 
+import com.enjoytrip.product.entity.Product;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,4 +14,8 @@ import lombok.Setter;
 public class ProductComment extends Comment {
 
     private Integer star;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
