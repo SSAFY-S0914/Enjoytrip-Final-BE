c43fed0871cc47cb52767188cc7f3f6494906702
이남곤 <nglee.dev@gmail.com>
2023-05-19 10:42:44 +0900

    feat: CommentService

--- src/main/java/com/enjoytrip/comment/service/CommentService.java
+++ src/main/java/com/enjoytrip/comment/service/CommentService.java
@@ -1,4 +1,64 @@
 package com.enjoytrip.comment.service;
 
+import com.enjoytrip.comment.dto.PostCommentDto;
+import com.enjoytrip.comment.dto.ProductCommentDto;
+import com.enjoytrip.comment.entity.PostComment;
+import com.enjoytrip.comment.entity.ProductComment;
+import com.enjoytrip.comment.mapper.CommentMapper;
+import com.enjoytrip.comment.repository.PostCommentRepository;
+import com.enjoytrip.comment.repository.ProductCommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+
+    private final PostCommentRepository postCommentRepository;
+    private final ProductCommentRepository productCommentRepository;
+    private final CommentMapper commentMapper;
+
+    public List<PostComment> findAllByPostId(Long postId) {
+        return postCommentRepository.findByPost_Id(postId);
+    }
+
+    public PostComment findPostCommentById(Long commentId) {
+        return postCommentRepository.findById(commentId).get();
+    }
+
+    public void createPostComment(PostComment postComment) {
+        postCommentRepository.save(postComment);
+    }
+
+    public void updatePostComment(PostComment postComment, PostCommentDto.Patch patchRequest) {
+        commentMapper.patchRequestToPostComment(patchRequest, postComment);
+        postCommentRepository.save(postComment);
+    }
+
+    public void deletePostCommentById(Long commentId) {
+        postCommentRepository.deleteById(commentId);
+    }
+
+    public List<ProductComment> findAllByProductId(Long productId) {
+        return productCommentRepository.findByProduct_Id(productId);
+    }
+
+    public ProductComment findProductCommentById(Long commentId) {
+        return productCommentRepository.findById(commentId).get();
+    }
+
+    public void createProductComment(ProductComment productComment) {
+        productCommentRepository.save(productComment);
+    }
+
+    public void updateProductComment(ProductComment productComment, ProductCommentDto.Patch patchRequest) {
+        commentMapper.patchRequestToProductComment(patchRequest, productComment);
+        productCommentRepository.save(productComment);
+    }
+
+    public void deleteProductCommentById(Long commentId) {
+        productCommentRepository.deleteById(commentId);
+    }
 }
