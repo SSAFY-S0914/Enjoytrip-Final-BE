dc8efd6b725c98c29890be83afab0714f9b2bcba
이남곤 <nglee.dev@gmail.com>
2023-05-19 09:50:26 +0900

    feat: CommentMapper

--- src/main/java/com/enjoytrip/comment/mapper/CommentMapper.java
+++ src/main/java/com/enjoytrip/comment/mapper/CommentMapper.java
@@ -1,4 +1,37 @@
 package com.enjoytrip.comment.mapper;
 
-public class CommentMapper {
+import com.enjoytrip.comment.dto.PostCommentDto;
+import com.enjoytrip.comment.dto.ProductCommentDto;
+import com.enjoytrip.comment.entity.PostComment;
+import com.enjoytrip.comment.entity.ProductComment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    @Mapping(source = "id", target = "commentId")
+    @Mapping(source = "writer.nickname", target = "writer")
+    @Mapping(source = "writer.id", target = "writerId")
+    PostCommentDto.Get postCommentToGetRequest(PostComment postComment);
+
+    List<PostCommentDto.Get> postCommentToGetRequest(List<PostComment> postComment);
+
+    PostComment postRequestToPostComment(PostCommentDto.Post postRequest);
+
+    PostComment postRequestToPatchComment(PostCommentDto.Patch patchRequest);
+
+    @Mapping(source = "id", target = "commentId")
+    @Mapping(source = "writer.nickname", target = "writer")
+    @Mapping(source = "writer.id", target = "writerId")
+    ProductCommentDto.Get productCommentToGetRequest(ProductComment productComment);
+
+    List<ProductCommentDto.Get> productCommentToGetRequest(List<ProductComment> productComment);
+
+    ProductComment postRequestToProductComment(ProductCommentDto.Post postRequest);
+
+    ProductComment patchRequestToProductComment(ProductCommentDto.Patch patchRequest);
+
 }
