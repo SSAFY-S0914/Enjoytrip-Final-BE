6afc16e86e3604ab18df5c235f61c4632dad8df6
Ha Jung Ho <ggk14@naver.com>
2023-05-19 17:46:45 +0900

    feat: Verification_Jwt 필터 구현

--- src/main/java/com/enjoytrip/auth/filter/JwtVerificationFilter.java
+++ src/main/java/com/enjoytrip/auth/filter/JwtVerificationFilter.java
@@ -1,7 +1,9 @@
 package com.enjoytrip.auth.filter;
 
-import com.enjoytrip.auth.CustomAuthorityUtils;
-import com.enjoytrip.auth.JwtTokenizer;
+import com.enjoytrip.auth.utils.CustomAuthorityUtils;
+import com.enjoytrip.auth.jwt.JwtTokenizer;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,7 +11,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -22,27 +23,35 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtVerificationFilter extends OncePerRequestFilter {
     private final JwtTokenizer jwtTokenizer; //JWT 검증 및 Claims 추출
     private final CustomAuthorityUtils authorityUtils; //검증에 성공 후 Authentication 객체에 사용자 권한 채우기
-
-    /*
-    1.
-     */
+    
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
-        Map<String, Object> claims = verifyJws(request); // JWT 검증 (JWS; JWT Signed)
-        setAuthenticationToContext(claims); // SecurityContext 에 Authentication 객체 저장
+        try {
+            Map<String, Object> claims = verifyJws(request); // JWT 검증 (JWS; JWT Signed)
+            setAuthenticationToContext(claims); // SecurityContext 에 Authentication 객체 저장
+        } catch (SignatureException se) { // 예외 발생 시 필터를 타고타고 진행 후 AuthenticationEntryPoint 에 도달
+            request.setAttribute("exception", se);
+        } catch (ExpiredJwtException ee) {
+            request.setAttribute("exception", ee);
+        } catch (Exception e) {
+            request.setAttribute("exception", e);
+        }
 
-        filterChain.doFilter(request, response);
+
+        filterChain.doFilter(request, response); // 문제없이 필터가 진행됐을 경우, 다음 필터 호출
     }
 
     //shouldNotFilter : 특정 조건에 부합되면 다음 Filter 로 건너뛰게하는 Filter
+    //자격 증명이 필요하지 않은 리소스 요청이 들어온 경우 실행될 필터
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String authorization = request.getHeader("Authorization");
 
-        return authorization == null || !authorization.startsWith("Bearer");
+        return authorization == null || !authorization.startsWith("Bearer"); // 토큰이 없거나, 인증 타입이 "Bearer"아닌 경우 JwtVerificationFilter 를 건너뛴다.
     }
 
+    // request Header 에서 토큰을 추출하고, 해당 토큰을 검증 및 claims(유저 정보)를 추출하는 메소드
     private Map<String, Object> verifyJws(HttpServletRequest request) {
         String jws = request.getHeader("Authorization").replace("Bearer ", ""); // header 에 존재하는 Bearer 제거
         String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey()); // JwtTokenizer 객체 필드로 존재하는 secret 키 호출 및 인코딩 키 생성
