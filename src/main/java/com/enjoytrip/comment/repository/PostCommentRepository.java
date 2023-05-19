eef0367bbfefe3165dd73cee9b9a614b4e604f87
이남곤 <nglee.dev@gmail.com>
2023-05-19 09:52:13 +0900

    feat: CommentRepository

---
+++ src/main/java/com/enjoytrip/comment/repository/PostCommentRepository.java
@@ -0,0 +1,9 @@
+package com.enjoytrip.comment.repository;
+
+import com.enjoytrip.comment.entity.PostComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
+}
