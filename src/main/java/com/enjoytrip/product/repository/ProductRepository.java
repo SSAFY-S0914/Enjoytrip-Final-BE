200d330bbca0bb267259003fdf5a053230c9319d
Lee, Nam Gon <nglee.dev@gmail.com>
2023-05-20 18:42:27 +0900

    [이남곤] Product Entity 삭제 (#18)
    * env: application.yml 파일 수정
    
    * feat: Dto 생성 및 기본 openapi client 생성
    
    * refactor: RespBodyDto -> ResponseBodyDto
    
    * feat: ProductSearchController 껍데기 생성
    
    * feat: ProductSearchController 구현 및 ProductSearchService 생성
    
    * feat: ProductSearchService, TourApi 구현
    
    * fix: ProductDto 수정
    
    * fix: ProductDto 수정 및 ResponseWrapper 수정
    
    * feat: 상세 정보 조회
    
    * feat: AreaCode, SigugunCode Entity
    
    * feat: ContentType Entity
    
    * feat: ContentType -> Category 이름 변경, Controller, Service 생성
    
    * feat: Catogory 관련 구현 완료
    
    * feat: numOfRows 기본 40으로 설정
    
    * feat: ProductRepository, Entity
    
    * chore: Product 정리

--- src/main/java/com/enjoytrip/product/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enjoytrip.product.repository;
-
-import com.enjoytrip.product.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
