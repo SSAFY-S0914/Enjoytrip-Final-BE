b03a5a15153c1633fe7c80128890f64fa666eb8b
이남곤 <nglee.dev@gmail.com>
2023-05-22 15:55:52 +0900

    refactor: Post 하는 경우 MapStruct 변경

--- src/main/java/com/enjoytrip/comment/mapper/PostCommentMapper.java
+++ src/main/java/com/enjoytrip/comment/mapper/PostCommentMapper.java
@@ -2,21 +2,36 @@ package com.enjoytrip.comment.mapper;
 
 import com.enjoytrip.comment.dto.PostCommentDto;
 import com.enjoytrip.comment.entity.PostComment;
+import com.enjoytrip.member.entity.Member;
+import com.enjoytrip.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface PostCommentMapper {
+@RequiredArgsConstructor
+public abstract class PostCommentMapper {
+
+    private final MemberService memberService;
 
     @Mapping(source = "id", target = "commentId")
-    PostCommentDto.Get postCommentToGetRequest(PostComment postComment);
+    public abstract PostCommentDto.Get postCommentToGetRequest(PostComment postComment);
 
-    List<PostCommentDto.Get> postCommentToGetRequest(List<PostComment> postComment);
+    public abstract List<PostCommentDto.Get> postCommentToGetRequest(List<PostComment> postComment);
 
-    PostComment postRequestToPostComment(PostCommentDto.Post postRequest);
+    @Mapping(target = "writer", source = "postRequest.writerId", qualifiedByName = "mapMember")
+    public abstract PostComment postRequestToPostComment(PostCommentDto.Post postRequest);
 
-    void patchRequestToPostComment(PostCommentDto.Patch patchRequest, @MappingTarget PostComment postComment);
+    public abstract void patchRequestToPostComment(PostCommentDto.Patch patchRequest, @MappingTarget PostComment postComment);
+
+
+    @Named("mapMember")
+    protected Member mapMember(Long memberId) {
+        return memberService.findOneMember(memberId);
+    }
+
 }
--- src/main/java/com/enjoytrip/comment/mapper/ProductCommentMapper.java
+++ src/main/java/com/enjoytrip/comment/mapper/ProductCommentMapper.java
@@ -2,21 +2,34 @@ package com.enjoytrip.comment.mapper;
 
 import com.enjoytrip.comment.dto.ProductCommentDto;
 import com.enjoytrip.comment.entity.ProductComment;
+import com.enjoytrip.member.entity.Member;
+import com.enjoytrip.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface ProductCommentMapper {
+@RequiredArgsConstructor
+public abstract class ProductCommentMapper {
+
+    private final MemberService memberService;
 
     @Mapping(source = "id", target = "commentId")
-    ProductCommentDto.Get productCommentToGetRequest(ProductComment productComment);
+    public abstract ProductCommentDto.Get productCommentToGetRequest(ProductComment productComment);
 
-    List<ProductCommentDto.Get> productCommentToGetRequest(List<ProductComment> productComment);
+    public abstract List<ProductCommentDto.Get> productCommentToGetRequest(List<ProductComment> productComment);
 
-    ProductComment postRequestToProductComment(ProductCommentDto.Post postRequest);
+    @Mapping(target = "writer", source = "postRequest.writerId", qualifiedByName = "mapMember")
+    public abstract ProductComment postRequestToProductComment(ProductCommentDto.Post postRequest);
 
-    void patchRequestToProductComment(ProductCommentDto.Patch patchRequest, @MappingTarget ProductComment productComment);
+    public abstract void patchRequestToProductComment(ProductCommentDto.Patch patchRequest, @MappingTarget ProductComment productComment);
+
+    @Named("mapMember")
+    protected Member mapMember(Long memberId) {
+        return memberService.findOneMember(memberId);
+    }
 }
