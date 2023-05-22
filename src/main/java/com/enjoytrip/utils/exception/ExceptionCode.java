a59e658d8c8d10c0cb018941884cbddae383225e
Lee, Nam Gon <nglee.dev@gmail.com>
2023-05-22 14:48:24 +0900

    [이남곤] 주석 해제 및 예외 처리 (#24)
    * env: application.yml 파일 수정
    
    * feat: Dto 생성 및 기본 openapi client 생성
    
    * refactor: RespBodyDto -> ResponseBodyDto
    
    * feat: ProductSearchController 껍데기 생성
    
    * feat: ProductSearchController 구현 및 ProductSearchService 생성
    
    * feat: ProductSearchService, TourApi 구현
    
    * fix: ProductDto 수정
    
    * fix: ProductDto 수정 및 ResponseWrapper 수정
    
    * feat: 상세 정보 조회
    
    * feat: AreaCode, SigugunCode Entity
    
    * feat: ContentType Entity
    
    * feat: ContentType -> Category 이름 변경, Controller, Service 생성
    
    * feat: Catogory 관련 구현 완료
    
    * feat: numOfRows 기본 40으로 설정
    
    * feat: ProductRepository, Entity
    
    * chore: Product 정리
    
    * fix: 모든 주석 해제
    
    * fix: exception처리

--- src/main/java/com/enjoytrip/utils/exception/ExceptionCode.java
+++ src/main/java/com/enjoytrip/utils/exception/ExceptionCode.java
@@ -1,16 +1,17 @@
-package com.enjoytrip.utils.businessLogicException;
+package com.enjoytrip.utils.exception;
 
 import lombok.Getter;
 
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
-    MEMBER_REDIRECTION_LOGIN_SUCCESS(303,"Member redirection login success"),
-    MEMBER_REDIRECTION_FIND_PASSWORD(303,"Member redirection find password"),
+    MEMBER_REDIRECTION_LOGIN_SUCCESS(303, "Member redirection login success"),
+    MEMBER_REDIRECTION_FIND_PASSWORD(303, "Member redirection find password"),
     MEMBER_NAME_EXISTS(409, "Member name exist"),
     MEMBER_UNAUTHORIZED(401, "Member unauthorized"),
     ANSWER_NOT_FOUND(404, "Answer not found"),
-    QUESTION_NOT_FOUND(404, "Question not found");
+    QUESTION_NOT_FOUND(404, "Question not found"),
+    NO_REQUIRED_PARAM(400, "No Required Parameters");
 
     @Getter
     private int status;
