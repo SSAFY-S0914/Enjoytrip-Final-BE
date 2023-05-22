a854d6c197f6815c21648631190bb1c71b889608
이남곤 <nglee.dev@gmail.com>
2023-05-23 02:08:36 +0900

    refactor: Mapper 내에서 연관관계 주입

--- src/main/java/com/enjoytrip/post/mapper/PostMapper.java
+++ src/main/java/com/enjoytrip/post/mapper/PostMapper.java
@@ -1,26 +1,47 @@
 package com.enjoytrip.post.mapper;
 
+import com.enjoytrip.group.entity.Group;
+import com.enjoytrip.group.service.GroupService;
+import com.enjoytrip.member.entity.Member;
+import com.enjoytrip.member.service.MemberService;
 import com.enjoytrip.post.dto.PostDto;
 import com.enjoytrip.post.entity.Post;
+import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface PostMapper {
+@RequiredArgsConstructor
+public abstract class PostMapper {
+
+    private final MemberService memberService;
+    private final GroupService groupService;
+
+    public abstract PostDto.Get postToGetRequest(Post post);
+
+    public abstract List<PostDto.Get> postListToGetRequest(List<Post> posts);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "writer", ignore = true)
-    @Mapping(target = "group", ignore = true)
-    Post postRequestToPost(PostDto.Post postRequest);
+    @Mapping(target = "writer", source = "postRequest.writerId", qualifiedByName = "mapMember")
+    @Mapping(target = "group", source = "postRequest.groupId", qualifiedByName = "mapGroup")
+    public abstract Post postRequestToPost(PostDto.Post postRequest);
 
-    @Mapping(target = "writer", ignore = true)
-    @Mapping(target = "group", ignore = true)
-    Post patchRequestToPost(PostDto.Patch patchRequest);
+    @Mapping(target = "writer", source = "patchRequest.writerId", qualifiedByName = "mapMember")
+    @Mapping(target = "group", source = "patchRequest.groupId", qualifiedByName = "mapGroup")
+    public abstract void patchRequestToPost(PostDto.Patch patchRequest, @MappingTarget Post post);
 
-    @Mapping(source = "post.writer.nickname", target = "nickname")
-    PostDto.Get postToGetRequest(Post post);
+    @Named("mapMember")
+    protected Member mapMember(Long writerId) {
+        return memberService.findOneMember(writerId);
+    }
 
-    List<PostDto.Get> postListToGetRequest(List<Post> posts);
+    @Named("mapGroup")
+    protected Group mapGroup(Long groupId) {
+        return groupService.findById(groupId);
+    }
+
 }
