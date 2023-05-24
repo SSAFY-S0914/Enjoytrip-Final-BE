0ab15f5c9e239824ec322da1e31201ede7e86892
Lee, Nam Gon <nglee.dev@gmail.com>
2023-05-24 09:34:03 +0900

    [이남곤] Repository 메소드 수정 (#35)
    * feat: comment entity 생성
    
    * feat: 대댓 기능 삭제
    
    * feat: CommentDto
    
    * fix: 오타 수정
    
    * feat: CommentMapper
    
    * feat: CommentRepository
    
    * feat: Comment와 Product, Post 연관관계
    
    * feat: Repository 연관관계로 검색
    
    * fix: 오타수정
    
    * fix: update Mapper
    
    * fix: 오타 수정
    
    * feat: CommentService
    
    * feat: Comment 구현 완료
    
    * fix: CommentDto.Get에 MemberDto.Get 반환
    
    * refactor: CommentMapper를 두개의 Mapper로 나눔
    
    * refactor: Post 하는 경우 MapStruct 변경
    
    * refactor: 하나의 CommentController를 PostCommentController와 ProductCommentController로 분리
    
    * refactor: Service 공통 부분 인터페이스로 추상화
    
    * fix: 오타 수정
    
    * refactor: Patch 부분 상속으로 구현
    
    * refactor: ProductCommentController
    
    * refactor: ProductCommentController
    
    * refactor: Update하는 경우, id로 업데이트 대상을 찾는 과정을 Service에서 처리
    
    * fix: Repository 메소드 수정

--- src/main/java/com/enjoytrip/comment/repository/ProductCommentRepository.java
+++ src/main/java/com/enjoytrip/comment/repository/ProductCommentRepository.java
@@ -9,8 +9,8 @@ import java.util.List;
 @Repository
 public interface ProductCommentRepository extends JpaRepository<ProductComment, Long> {
 
-    List<ProductComment> findByProduct_Id(Long productId);
+    List<ProductComment> findByProductId(Long productId);
 
-    List<ProductComment> findByMember_Id(Long writerId);
+    List<ProductComment> findByWriter_Id(Long writerId);
 
 }
